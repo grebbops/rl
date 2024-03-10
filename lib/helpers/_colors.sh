9e035cad78491d2aaa736acd9dbc87c232fe5927
Dan Grebb <git@dgrebb.com>
2024-03-09 22:56:25 -0500

    feat(cli): add greeting and command scaffolding

---
+++ lib/helpers/_colors.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export BLUE="\e[94m"
+export NC="\033[0m" # No Color
---
+++ lib/helpers/hello.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo
+printf "${BLUE}   /\ ${NC}\n"
+printf "${BLUE}  (  ) ${NC}\n"
+printf "${BLUE}  (  ) ${NC}\n"
+printf "${BLUE} /|/\|\ ${NC}\n"
+printf "${BLUE}/_||||_\ ${NC}\n"
+echo
