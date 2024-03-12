f549e586d5abed59689b179168b7f863da5126dc
Dan Grebb <git@dgrebb.com>
2024-03-11 20:36:56 -0400

    refactor(cli): move rl into separate directory

--- lib/helpers/_index.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env
-
-source $LIB/helpers/_colors.sh
-
-hello() {
-  $LIB/helpers/$FUNCNAME.sh
-}
---
+++ rl/lib/helpers/help.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+############################################################
+# rl Help                                                  #
+############################################################
+echo "Each utility is defined by its API:"
+echo
+echo "jira (j) []"
+echo
+echo "Each workflow has a name and shorthand:"
+echo
+echo "release-start (r) [--name, -n|--description, --desc, -d|--start, -s|--end -e]"
+echo
+echo
+echo "Syntax: scriptTemplate [-g|h|v|V]"
+echo "options:"
+echo "g     Print the GPL license notification."
+echo "h     Print this Help."
+echo "v     Verbose mode."
+echo "V     Print software version and exit."
+echo
