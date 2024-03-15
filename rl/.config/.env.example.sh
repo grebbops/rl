8215e1b6eb6f4dc364c8b7357c414c14b43ca497
Dan Grebb <git@dgrebb.com>
2024-03-15 15:50:04 -0400

    docs: add example .env for rl config

---
+++ rl/.config/.env.example.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export JIRA_URL=https://account.atlassian.net
+export JIRA_API_URL=https://account.atlassian.net/rest/api/3
+export JIRA_API_USER=email@example.com
+export JIRA_API_TOKEN=
+export JIRA_PROJECT_ID=10000
+export CONFLUENCE_URL=https://account.atlassian.net/wiki
+export CONFLUENCE_API_URL=https://account.atlassian.net/wiki/api/v2
+export CONFLUENCE_API_USER=email@example.com
+export CONFLUENCE_API_TOKEN=
+export CONFLUENCE_SPACE_ID=
+export RALLY_API_URL=
+export RALLY_API_TOKEN=
+export TEAMS_WEBHOOK=
